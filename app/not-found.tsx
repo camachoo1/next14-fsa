12688ecc24695c33ab8ef23ed25e11e9e61fcc2d
“Omar <ocamacho812@gmail.com>
2024-02-29 00:58:25 -0800

    Create a not-found.tsx page

---
+++ app/not-found.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import Image from "next/image";
+
+export default function NotFound() {
+  return (
+    <div className="m-auto flex w-full flex-col items-center justify-center py-20">
+      <div className="m-auto flex flex-col items-center space-y-1">
+        <Image src="/oops.webp" alt="oops" height={100} width={100} />
+        <div className="flex flex-col items-center gap-4">
+          <h1 className="text-3xl font-bold uppercase tracking-wider">
+            There's nothing here
+          </h1>
+          <p className="tracking-tighter text-muted-foreground">
+            Sorry, we couldn't find the page you were looking for. Want to see
+            the latest?
+          </p>
+        </div>
+      </div>
+
+      <div className="flex p-10">
+        <button className="mx-2 flex items-center text-nowrap bg-black px-4 py-3 text-xs uppercase text-white">
+          <a className="" href="/">
+            Go to the home page
+          </a>
+        </button>
+        <button className="mx-2 flex items-center text-nowrap bg-black px-4 py-3 text-xs uppercase text-white">
+          Shop new arrivals
+        </button>
+      </div>
+    </div>
+  );
+}
