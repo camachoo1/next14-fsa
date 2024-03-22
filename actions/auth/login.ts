30c273f2ad2df4730d4cee6a2a5deec95035f64d
Omar Camacho <116383442+camachoo1@users.noreply.github.com>
2024-03-21 23:58:58 -0700

    finish login server action

--- actions/auth/login.ts
+++ actions/auth/login.ts
@@ -3,11 +3,53 @@
 import { LoginSchema, RegisterSchema } from "@/app/types";
 import { z } from "zod";
 import * as argon from "argon2";
+import { db } from "@/lib/db/db";
+import { lucia } from "@/lib/auth/auth";
+import { cookies } from "next/headers";
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
   try {
-    const vals = LoginSchema.parse(values);
-    return console.log({ vals });
+    LoginSchema.parse(values);
+
+    const existingUser = await db.user.findUnique({
+      where: { email: values.email },
+    });
+    if (!existingUser) {
+      return {
+        error:
+          "There was an issue with your login attempt. If you originally signed up with a social account, please use that method to log in. Otherwise, please check your credentials and try again.",
+      };
+    }
+
+    if (!existingUser.hashedPassword) {
+      return {
+        error:
+          "There was an issue with your login attempt. If you originally signed up with a social account, please use that method to log in. Otherwise, please check your credentials and try again.",
+      };
+    }
+
+    const validPassword = await argon.verify(
+      existingUser.hashedPassword,
+      values.password,
+    );
+    console.log({ validPassword });
+
+    if (!validPassword) {
+      return {
+        error: "Invalid username or password",
+      };
+    }
+
+    const session = await lucia.createSession(existingUser.id, {});
+    const sessionCookie = lucia.createSessionCookie(session.id);
+    cookies().set(
+      sessionCookie.name,
+      sessionCookie.value,
+      sessionCookie.attributes,
+    );
+    return {
+      success: "Login successful",
+    };
   } catch (err: any) {
     return {
       error: err?.message,
