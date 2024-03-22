ebd77ed5826325f496231a6cefc54ae491118072
Omar Camacho <116383442+camachoo1@users.noreply.github.com>
2024-03-21 23:48:47 -0700

    Update login method

--- actions/auth/login.ts
+++ actions/auth/login.ts
@@ -1,77 +1,18 @@
 "use server";
 
-import { lucia } from "@/lib/auth/auth";
-import { db } from "@/lib/db/db";
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
-import { Argon2id } from "oslo/password";
+import { LoginSchema, RegisterSchema } from "@/app/types";
+import { z } from "zod";
+import * as argon from "argon2";
 
-interface ActionResult {
-  error: string | null;
-}
-
-const login = async (_: any, formData: FormData): Promise<ActionResult> => {
-  const email = formData.get("email");
-  if (typeof email !== "string") {
-    return {
-      error:
-        "There was an issue with your login attempt. If you originally signed up with a social account, please use that method to log in. Otherwise, please check your credentials and try again.",
-    };
-  }
-
-  const password = formData.get("password");
-  if (typeof password !== "string") {
-    return {
-      error:
-        "There was an issue with your login attempt. If you originally signed up with a social account, please use that method to log in. Otherwise, please check your credentials and try again.",
-    };
-  }
-  const argon = new Argon2id();
-
+export const login = async (values: z.infer<typeof LoginSchema>) => {
   try {
-    const existingUser = await db.user.findUnique({
-      where: { email: email.toLowerCase() },
-    });
-
-    if (existingUser && !existingUser.hashedPassword) {
-      const oAuthAccounts = await db.oAuthAccount.findMany({
-        where: { userId: existingUser.id },
-      });
-      if (oAuthAccounts.length > 0) {
-        return {
-          error:
-            "There was an issue with your login attempt. If you originally signed up with a social account, please use that method to log in. Otherwise, please check your credentials and try again.",
-        };
-      }
-    }
-
-    const dummy = process.env.DUMMY_PASSWORD as string;
-    const dummyHash = await argon.hash(dummy);
-    const validPassword =
-      existingUser && existingUser.hashedPassword
-        ? await argon.verify(existingUser.hashedPassword, password)
-        : await argon.verify(dummyHash, password);
-
-    if (!validPassword || !existingUser) {
-      return {
-        error:
-          "There was an issue with your login attempt. If you originally signed up with a social account, please use that method to log in. Otherwise, please check your credentials and try again.",
-      };
-    }
-
-    const session = await lucia.createSession(existingUser.id, {});
-    const sessionCookie = lucia.createSessionCookie(session.id);
-    cookies().set(
-      sessionCookie.name,
-      sessionCookie.value,
-      sessionCookie.attributes,
-    );
-    return redirect("/");
+    const vals = LoginSchema.parse(values);
+    return console.log({ vals });
   } catch (err: any) {
     return {
-      error: err.message || "An error occurred",
+      error: err?.message,
     };
   }
 };
 
-export default login;
+export const register = async (values: z.infer<typeof RegisterSchema>) => {};
--- app/types/index.ts
+++ app/types/index.ts
@@ -6,3 +6,19 @@ export const LoginSchema = z.object({
     .string()
     .min(8, { message: "Password must be at least 8 characters." }),
 });
+
+export const RegisterSchema = z
+  .object({
+    name: z.string().min(4, { message: "Name must be at least 4 characters" }),
+    email: z.string().email({ message: "Please enter a valid email." }),
+    password: z
+      .string()
+      .min(8, { message: "Password must be at least 8 characters." }),
+    confirmPassword: z
+      .string()
+      .min(8, { message: "Password must be at least 8 characters." }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+  });
