b87ec7819b6a4f8c55d01805a22c424656e96919
Omar Camacho <116383442+camachoo1@users.noreply.github.com>
2024-07-12 16:12:41 -0700

    Comment out unused code for orders

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -2,32 +2,32 @@ import AccountMenu from "@/app/account/_components/account-menu";
 import { validateRequest } from "@/lib/auth/auth";
 import DynamicContent from "./_components/DynamicContent";
 
-const orders = [
-  {
-    id: "1",
-    date: "January, 17, 2024",
-    total: "$85.32",
-    details: {
-      name: "Double Take Short Sleeve",
-      color: "Black",
-      size: "L",
-      quantity: 1,
-      price: "$78",
-    },
-  },
-  {
-    id: "2",
-    date: "January, 17, 2024",
-    total: "$85.32",
-    details: {
-      name: "Double Take Short Sleeve",
-      color: "Bone",
-      size: "M",
-      quantity: 1,
-      price: "$78",
-    },
-  },
-]; 
+// const orders = [
+//   {
+//     id: "1",
+//     date: "January, 17, 2024",
+//     total: "$85.32",
+//     details: {
+//       name: "Double Take Short Sleeve",
+//       color: "Black",
+//       size: "L",
+//       quantity: 1,
+//       price: "$78",
+//     },
+//   },
+//   {
+//     id: "2",
+//     date: "January, 17, 2024",
+//     total: "$85.32",
+//     details: {
+//       name: "Double Take Short Sleeve",
+//       color: "Bone",
+//       size: "M",
+//       quantity: 1,
+//       price: "$78",
+//     },
+//   },
+// ]; 
 
 export default async function AccountPage() {
   const { user } = await validateRequest();
