041dbba95c446a78953089fec3f889dbf50ee0f0
Omar Camacho <116383442+camachoo1@users.noreply.github.com>
2024-07-26 20:09:43 -0700

    Comment out code that isnt important

--- components/order-history.tsx
+++ components/order-history.tsx
@@ -1,31 +1,35 @@
-{
-  orders.map((order) => (
-    <div key={order.id} className="mb-4 p-4 shadow-lg">
-      <div className="mb-4 flex items-center justify-between">
-        <div>
-          <p className="uppercase">Total</p>
-          <p>{order.total}</p>
-        </div>
-        <div>
-          <p>Order Placed</p>
-          <p>{order.date}</p>
-        </div>
-        <div>
-          <p>Order#</p>
-          <p>{order.id}</p>
-        </div>
-        <div className="flex items-center">
-          <div>
-            <p>{order.details.name}</p>
-            <p>Color: {order.details.color}</p>
-            <p>Size: {order.details.size}</p>
-            <p>Quantity: {order.details.quantity}</p>
-          </div>
-          <div className="ml-auto">
-            <p>{order.details.price}</p>
-          </div>
-        </div>
-      </div>
-    </div>
-  ));
-}
+/*
+NEED TO FIGURE OUT HOW TO ADD ORDERS TO INTEGRATE THIS COMPONENT
+*/
+
+// {
+//   orders.map((order) => (
+//     <div key={order.id} className="mb-4 p-4 shadow-lg">
+//       <div className="mb-4 flex items-center justify-between">
+//         <div>
+//           <p className="uppercase">Total</p>
+//           <p>{order.total}</p>
+//         </div>
+//         <div>
+//           <p>Order Placed</p>
+//           <p>{order.date}</p>
+//         </div>
+//         <div>
+//           <p>Order#</p>
+//           <p>{order.id}</p>
+//         </div>
+//         <div className="flex items-center">
+//           <div>
+//             <p>{order.details.name}</p>
+//             <p>Color: {order.details.color}</p>
+//             <p>Size: {order.details.size}</p>
+//             <p>Quantity: {order.details.quantity}</p>
+//           </div>
+//           <div className="ml-auto">
+//             <p>{order.details.price}</p>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   ));
+// }
